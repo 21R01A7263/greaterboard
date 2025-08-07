35c92fc524dc1dccfcb346ed8ea8e3a58899a567
21R01A7263 <21R01A7263@cmritonline.ac.in>
2025-08-07 11:04:51 +0530

    redirect URL

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,9 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <SignIn />
+      <SignIn fallbackRedirectUrl="/" />
     </div>
   )
 }
