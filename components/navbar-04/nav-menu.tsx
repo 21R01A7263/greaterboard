459c0fdb99799d30577d251c4e36db7e2f18ec51
21R01A7263 <21R01A7263@cmritonline.ac.in>
2025-08-08 10:42:35 +0530

    flex col

--- components/navbar-04/nav-menu.tsx
+++ components/navbar-04/nav-menu.tsx
@@ -3,19 +3,21 @@ import {
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-} from "@/components/ui/navigation-menu";
-import { NavigationMenuProps } from "@radix-ui/react-navigation-menu";
-import Link from "next/link";
+} from '@/components/ui/navigation-menu';
+import { NavigationMenuProps } from '@radix-ui/react-navigation-menu';
+import Link from 'next/link';
 
 export const NavMenu = (props: NavigationMenuProps) => (
   <NavigationMenu {...props}>
-    <NavigationMenuList className="gap-6 space-x-0 scale-110 data-[orientation=vertical]:flex-col font-semibold data-[orientation=vertical]:items-start">
+    <NavigationMenuList className='gap-6 space-x-0 scale-110 data-[orientation=vertical]:flex-col font-semibold data-[orientation=vertical]:items-start'>
       <NavigationMenuItem>
         <NavigationMenuLink asChild>
-          <Link href="/profile">Profile</Link>
+          <Link href='/profile'>Profile</Link>
         </NavigationMenuLink>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
         <NavigationMenuLink asChild>
-          <Link href="/admin">Admin</Link>
+          <Link href='/admin'>Admin</Link>
         </NavigationMenuLink>
       </NavigationMenuItem>
     </NavigationMenuList>
